07699bf5395e2750e2b68312cfc8f634a0c26c14
Samah Alsabbagh <samah.alsabbaghs@gmail.com>
2026-09-16 15:25:50 +0300

    Add custom user model foundation

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+  phone = models.CharField(max_length=20, blank=True)
+  is_email_verified = models.BooleanField(default=False)
+  class Role (models.TextChoices):
+    CUSTOMER = "CUSTOMER", "Customer"
+    OWNER = "OWNER", "Owner"
+    ADMIN = "ADMIN", "Admin"
+    STAFF = "STAFF", "Staff"
+  role = models.CharField(max_length=20, choices=Role.choices,default=Role.CUSTOMER,)
+  username = None
+  email = models.EmailField(unique=True)
+  USERNAME_FIELD = "email"
+  REQUIRED_FIELDS = []
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -40,7 +40,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Our apps
+    'accounts',
 ]
+
+AUTH_USER_MODEL = 'accounts.User'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
