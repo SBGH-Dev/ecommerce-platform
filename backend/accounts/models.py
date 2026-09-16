80c2b64110393f8e1bf9dc39195a56a747e8112a
Samah Alsabbagh <samah.alsabbaghs@gmail.com>
2026-09-16 16:32:37 +0300

    Add custom email user manager

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,5 +1,19 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
+
+
+class CustomUserManager(UserManager):
+    def create_user(self, email, password=None, **extra_fields):
+      email = self.normalize_email(email)
+      user = self.model(email=email, **extra_fields)
+      user.set_password(password)
+      user.save()
+      return user
+
+    def create_superuser (self, email, password=None, **extra_fields):
+      extra_fields.setdefault("is_staff", True)
+      extra_fields.setdefault("is_superuser", True)
+      return self.create_user(email, password, **extra_fields)
 
 class User(AbstractUser):
   phone = models.CharField(max_length=20, blank=True)
@@ -14,3 +28,4 @@ class User(AbstractUser):
   email = models.EmailField(unique=True)
   USERNAME_FIELD = "email"
   REQUIRED_FIELDS = []
+  objects = CustomUserManager()
