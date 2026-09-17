1bbdf7c5dfef683802c18b2ced2ddf0fc873cac1
Samah Alsabbagh <samah.alsabbaghs@gmail.com>
2026-09-17 13:33:23 +0300

    StorageSettings Model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -3,27 +3,27 @@ from django.contrib.auth.models import AbstractUser, UserManager
 
 
 class CustomUserManager(UserManager):
-    def create_user(self, email, password=None, **extra_fields):
+    def create_user(self, email, password=None, **kwargs):
       if not email:
         raise ValueError("Email Is Required")
       
       email = self.normalize_email(email)
-      user = self.model(email=email, **extra_fields)
+      user = self.model(email=email, **kwargs)
       user.set_password(password)
       user.save()
       return user
 
-    def create_superuser (self, email, password=None, **extra_fields):
-      extra_fields.setdefault("is_staff", True)
-      extra_fields.setdefault("is_superuser", True)
-      
-      if extra_fields.get("is_staff") is not True:
+    def create_superuser (self, email, password=None, **kwargs):
+      kwargs.setdefault("is_staff", True)
+      kwargs.setdefault("is_superuser", True)
+
+      if kwargs.get("is_staff") is not True:
         raise ValueError("is_staff must be ture")
             
-      if extra_fields.get("is_superuser") is not True:
+      if kwargs.get("is_superuser") is not True:
         raise ValueError("is_superuser must be ture")
       
-      return self.create_user(email, password, **extra_fields)
+      return self.create_user(email, password, **kwargs)
 
 class User(AbstractUser):
   phone = models.CharField(max_length=20, blank=True)
--- backend/store/models.py
+++ backend/store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 class StoreSettings(models.Model):
   store_name = models.CharField(max_length=150)
@@ -6,3 +7,19 @@ class StoreSettings(models.Model):
   phone = models.CharField(max_length=20, blank=True)
   currency = models.CharField(max_length=3, default="SAR")
   address = models.TextField(blank=True)
+  description  = models.TextField(blank=True)
+  tax_enabled = models.BooleanField(default=False)
+  tax_percentage = models.DecimalField( max_digits=5 , decimal_places=2, default=0)
+  delivery_enabled = models.BooleanField(default=True)
+  minimum_order_amount = models.DecimalField( max_digits=10, decimal_places=2, default=0)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+  whatsapp = models.CharField(max_length=20, blank=True)
+  primary_color = models.CharField(max_length=7, blank=True)
+  secondary_color = models.CharField(max_length=7, blank=True)    
+
+  def save(self, *args, **kwargs):
+    if not self.pk and StoreSettings.objects.exists():
+      raise ValidationError("StoreSettings can only be created once.")
+
+    super().save(*args, **kwargs)
